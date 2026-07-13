6811357d9c4ed72c63d2f0336e85c61aedcc9cc0
yugal tiwari <yugaltiwari70@gmail.com>
2026-07-13 13:15:04 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,374 @@
+//   console.log(" welcome to apna college");
+// //  console.log("yugal tiwari");
+// //  let name="yugal tiwari";
+// //   console.log(name);
+// //  let firstNAME='yugal',lastName='tiwari';
+// // console.log(lastName);
+// // let name='yugal';
+// //   let age=30;
+// //  let isaproved=false;
+// // let firstname=userdefined;
+// // let selectedcolors=['red','blue'];
+// // seletedcolors[2]=1;
+// // console.log(selectedcolors.length);
+// // function greet(name){
+// //     console.log('helooworldd'+name);
+// // }
+// // greet('yugal');
+
+// // 1 .console api
+// //  console.log(yugaltiwari);
+// // console.clear("yugal tiwwari");
+// // console.warn("thie is warn");
+// // console.error("this is error");
+
+// //2 javascript variable
+// // var number1 =44;
+// // var number2 =44;
+// // console.log(number1+number2);
+// // let name = "yugal";
+// // console.log(name);
+// // const n = 3.14;
+// // console.log(n);
+
+// // 4.data type
+// // var num1 = 455;
+// // var num1 = 45.5;
+// // //string
+// // var str1 = "this is a string";
+// // var str2 = "this is a  two string";
+// // //objects
+// // var marks = {
+// //     ravi:34,
+// //     subham :4
+// // }
+// // // console.log(marks);
+// // //boleans
+// // var a = true;
+// // var b= false;
+// // // console.log(a,b);
+// // //undefined
+// // var und;
+// // // console.log(und);
+// // //null
+// // var n= null;
+// // console.log(n);
+
+// // two type of data type
+// // 1.primitive data type: undefined,null,number,string,boolean,Symbol
+// // 2.reference data type:arrays ,object
+// // var arr =[1,2,3,4,5];
+// // console.log(arr[3])
+
+// // 5.operator 
+// // var a=4;
+// // var b =4;
+// // console.log("the value of a+b is:",a+b);
+// // console.log("the value of a-b is:",a-b);
+// // console.log("the value of a*b is:",a*b);
+// // console.log("the value of a/b is:",a/b);
+// // console.log("the value of a%b is:",a%b);
+
+// //assignment
+
+// // var c=b;
+// // console.log(b);
+// //  c+=2;
+// // console.log(c);
+// //  c-=2;
+// // c*=2;
+// //  c/=2;
+// // console.log(c);
+
+// //comparision operator
+// // var x =34;
+// // var y=56;
+// // console.log(x==y);
+// // console.log(x>=y);
+// // console.log(x<=y);
+// // console.log(x>y);
+// // console.log(x<y);
+
+// //logical operator
+// // console.log(true && false)
+// // console.log(false && false)
+// // console.log(false && true)
+
+// // console.log(true || false)
+// // console.log(false || false)
+// // console.log(false || true)
+// // console.log(true || false)
+
+// // console.log( ! false)
+// // console.log(! false)
+// // console.log(! true)
+
+// //function dobara padhna
+// // function avg(a,b){
+// //    let c =(a+b)/2;
+// //     return c;
+// // }
+// //  let c1 = avg(4,72);
+// //  let c2 = avg(5,5);
+// // console.log(c1,c2);
+
+// // //if else
+// // var age =7;
+// // if(age>8){
+// //     console.log("you are  not kid");
+// // }
+// // else if(age<=0){
+// //     console.log("you are  not born");
+// // }
+// // else{
+// //     console.log("you are kid")
+// // }
+
+// //loops
+
+// //  var arr = [1,2,3,9,5];
+// // console.log(arr);
+// // // for(var i=0;i<arr.length;i++){
+// // //     console.log(arr[i])
+// // // }
+// // arr.forEach(function(element){
+// //     console.log(element);
+// // })
+// //while
+// //  let j=0;
+// // while(j<arr.length){
+// //   console.log(arr[j]);
+// //   j++;
+// // }
+// // do{
+// //   console.log(arr[j]);
+// //   j++;
+// // }
+// // while(j<arr.length);
+
+
+
+
+// // practice
+// // let num = 20;
+// // let fact =1;
+// // for(var i=1;i<=5;i++){
+// //   fact = fact*i;
+// //   }
+// // console.log("factorial= ",fact);
+//  //break
+// //  var arr = [1,2,3,9,5];
+// // // console.log(arr);
+// //  for(var i=0;i<arr.length;i++){
+// //   if(i==3){
+// //    // break;
+// //     continue
+// //   }
+// //     console.log(arr[i])
+// //  }
+
+// //array method
+// // let myarr = ["fan","camera",65,null,34,true];
+// // console.log(myarr.length);
+// // myarr.push("yugal")
+// // console.log(myarr);
+
+// // myarr.shift()
+// // console.log(myarr)
+
+// // let newLen = myarr.unshift("yugal")
+// // console.log(newLen);
+// // console.log(myarr)
+
+// // myarr.sort()
+// // console.log(myarr)
+
+// //string method
+// // let String = "harry is a good boy good good harry";
+// // console.log(String.length);
+
+// // console.log(String.indexOf("boy"));
+// // console.log(String.lastIndexOf("harry"));
+// // console.log(String.slice(1,4));
+// // console.log(String.replace("harry","yugal"));
+// // console.log(String.replace("good","bad"));
+
+// //dates in script
+// // let myDate = new Date();
+// // // console.log(myDate)
+// // console.log(myDate.getTime());
+// // console.log(myDate.getFullYear());
+// // console.log(myDate.getDay());
+// // console.log(myDate.getMinutes());
+// // console.log(myDate.getHours());
+
+// //dom
+
+// // let elem = document.getElementById('click');
+// // console.log(elem)
+
+// // // let elemClass = document.getElementsByClassName("container")
+// // // console.log(elemClass)
+
+// // // elemClass[0].classList.add("big-primary")
+// // // elemClass[0].classList.add("text-success")
+// // // console.log(elem.innerHTML);
+// // // console.log(elem.innerText);
+
+// // // console.log(elemClass[0].innerHTML);
+// // // console.log(elemClass[0].innerText);
+// // tn = document.getElementsByTagName('div')
+// // console.log(tn)
+// // CreateElement =document.createdElement('p');
+// // createElement.innerText = "this is a created para";
+// // tn[0].appendChild(createdElement)
+
+// // CreatedElement2 =document.createdElement('b');
+// // CreatedElement2.innerText = "this is created bold";
+// //  tn[0].replaceChild(CreatedElement2,createdElement);
+// // sel = document.querySelector('container')
+// // console.log(sel)
+
+
+
+// //EVENTES
+// // function clicked(){
+// //   console.log('the button was clicked')
+// // }
+// // window.onload = function(){
+// //   console.log('the document was loaded')
+// // }
+// // firstContainer.addEventListener('click',function(){
+// // console.log("clicke on container")
+// // })
+// // firstContainer.addEventListener('mouseover',function(){
+// //   console.log("mouse in ")
+// // })
+
+// // firstContainer.addEventListener('mouseout',function(){
+// //   console.log("mouse out ")
+// // })
+//  function sum(a,b){
+//   return a+b;
+
+//  }
+
+
+let slider = document.getElementById("slider");
+
+let index = 0;
+const totalSlides = 3;
+
+setInterval(() => {
+    index++;
+
+    if (index >= totalSlides) {
+        index = 0;
+    }
+
+    slider.style.transform = `translateX(-${index * 100}%)`;
+
+}, 3000);
+let count = 0;
+
+const cartCount = document.getElementById("cart-count");
+const buttons = document.querySelectorAll(".cart-btn");
+const toast = document.getElementById("toast");
+
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+
+        e.stopPropagation();
+
+        count++;
+        cartCount.innerText = count;
+
+        const productName = button.closest(".card").querySelector("h4").innerText;
+
+        toast.innerText = `✅ ${productName} added to cart`;
+        toast.classList.add("show");
+
+        setTimeout(() => {
+            toast.classList.remove("show");
+        }, 2000);
+
+    });
+});
+
+
+const searchBox = document.getElementById("search-box");
+const cards = document.querySelectorAll(".card");
+
+searchBox.addEventListener("keyup", function () {
+
+    const searchValue = searchBox.value.toLowerCase();
+
+    cards.forEach(card => {
+
+        const productName = card.querySelector("h4").innerText.toLowerCase();
+
+        if (productName.includes(searchValue)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+
+    });
+
+});
+const themeBtn = document.getElementById("theme-btn");
+
+themeBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+});
+
+
+const modal = document.getElementById("productModal");
+
+const modalImg = document.getElementById("modal-img");
+const modalTitle = document.getElementById("modal-title");
+const modalPrice = document.getElementById("modal-price");
+const modalRating = document.getElementById("modal-rating");
+
+const closeBtn = document.querySelector(".close");
+
+cards.forEach(card=>{
+
+    card.addEventListener("click",()=>{
+
+        modal.style.display="flex";
+
+        modalImg.src=card.querySelector("img").src;
+        modalTitle.innerText=card.querySelector("h4").innerText;
+        modalPrice.innerText=card.querySelectorAll("p")[0].innerText;
+        modalRating.innerText=card.querySelector(".rating").innerText;
+
+    });
+
+});
+
+closeBtn.addEventListener("click",()=>{
+
+    modal.style.display="none";
+
+});
+
+window.addEventListener("click",(e)=>{
+
+    if(e.target===modal){
+
+        modal.style.display="none";
+
+    }
+
+});
+
+
+
+
+
+
+
+
